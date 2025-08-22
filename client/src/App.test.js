a8014737b0964da8a818c50faf6be2fca2fb6c66
burrows99 <raunakburrows@gmail.com>
2025-08-22 21:11:29 +0100

    Synthetic Patient Generation Updated

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders NHS MOA Triage header', () => {
-  render(<App />);
-  const heading = screen.getByText(/NHS MOA Triage/i);
-  expect(heading).toBeInTheDocument();
-});
